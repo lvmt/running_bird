ae38a9a168da61663a9befb04f7938d963899012
lvmt <13554221497@163.com>
2020-05-18 19:39:26 +0800

    shell 编程

---
+++ test.sh
@@ -0,0 +1,8 @@
+num1=100
+num2=100
+if test num1=num2
+then
+    echo 'The two strings are equal!'
+else
+    echo 'The two strings are not equal!'
+fi
---
+++ while.sh
@@ -0,0 +1,13 @@
+# COUNTER=0
+# while test ${COUNTER} -lt 5 
+# do
+#     COUNTER=`expr $COUNTER + 1`
+#     echo $COUNTER
+# done
+
+echo 'type <CTRL-D> to terminate'
+echo -n 'enter your most liked film: '
+while read FILM
+do
+    echo "Yeah! great film the $FILM"
+done
